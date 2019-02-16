0b2e5334778d983e0d5739bc1bad7c064b8abf5b
Jessica Hsueh <jessicahsueh@jchsueh.local>
2019-02-16 08:50:46 -0800

    Initial Commit

---
+++ TrashTrackerAlpha/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TrashTrackerAlpha
+//
+//  Created by Jessica Hsueh on 2/16/19.
+//  Copyright © 2019 Jessica Hsueh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TrashTrackerAlpha/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TrashTrackerAlpha
+//
+//  Created by Jessica Hsueh on 2/16/19.
+//  Copyright © 2019 Jessica Hsueh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
